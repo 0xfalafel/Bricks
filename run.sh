a63a27c4745d84896d40374ec19a666b51696346
Lasne Olivier <lasneolivier@gmail.com>
2024-09-18 11:37:59 +0200

    added a few comments to run.sh

--- run.sh
+++ run.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
 
+# activate the mysqli extension in the php config
 echo 'extension=mysqli' >> /etc/php/8.1/cli/php.ini
 
+# start the mysql service
 service mysql start
-
+# Create the database and our user
 mysql < /setup.sql
 
+# start apache2
 apachectl -D FOREGROUND
